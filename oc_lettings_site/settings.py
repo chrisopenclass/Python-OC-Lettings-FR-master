2f2beb90f4ea7a8952fbce5f4fca9f9200965dbf
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-07-17 19:53:04 +0200

    adding sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -109,3 +111,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+sentry_sdk.init(
+    dsn="https://40f3f37e97484b1e82cf3830d5354241@o1320221.ingest.sentry.io/6576257",
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+
+
+def trigger_error(request):
+    1 / 0
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('lettings/', include('lettings.urls')),
     path('profiles/', include('profiles.urls')),
+    path('sentry-debug/', trigger_error),
 ]
