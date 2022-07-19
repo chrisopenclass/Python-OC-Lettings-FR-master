dcbdea0c48cb074cb8fe6756f2b60dc906a55451
chrisopenclass <90398020+chrisopenclass@users.noreply.github.com>
2022-07-19 18:16:34 +0200

    ajout de commentaire

--- lettings/urls.py
+++ lettings/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+# definition des différentes url
+
 urlpatterns = [
     path('', views.lettings_index, name='lettings_index'),
     path('<int:letting_id>/', views.letting, name='letting'),
